54dccef3fb8874765caa289066eb38c762443200
Pache Ochoa <ochoazamorasergio247@gmail.com>
2021-05-06 10:48:05 -0500

    add function delete and update. add get courses of student logged

--- app/Controllers/Courses.php
+++ app/Controllers/Courses.php
@@ -5,9 +5,11 @@ namespace App\Controllers;
 use App\Models\Course;
 use App\Models\Student;
 use DateTime;
-
+session_start();
 class Courses extends BaseController
+
 {
+     
     public static function authorizationValidate()
     {
 
@@ -33,9 +35,9 @@ class Courses extends BaseController
                     $request->getHeader('Authorization') == 'Authorization: Basic '
                     . base64_encode($student['id_student'] . ":" . $student['secret_key'])
                 ) {
-                    session_start();
+                    
                     $_SESSION['id_student'] = $student['id'];
-                    $_SESSION['id_student'];
+                    
                     return true;
                 }
 
@@ -51,11 +53,19 @@ class Courses extends BaseController
 
     public function index()
     {
-        $CourseModel = new Course();
-
-        $courses = $CourseModel->findAll();
+        $db  = \Config\Database::connect();
+        
 
         $authorized =  Courses::authorizationValidate();
+        $id_student = $_SESSION['id_student'];
+        
+        $stm = $db->query("CALL sp_get_courses(".$id_student.")");
+        $courses = $stm->getResult();
+      
+
+       
+
+        
 
         
 
@@ -70,6 +80,8 @@ class Courses extends BaseController
             return json_encode(array("status" => 401, "message" => "Unauthorized"));
         }
     }
+
+
 
     public function create()
     {
@@ -133,4 +145,133 @@ class Courses extends BaseController
             return json_encode(array("status" => 401, "message" => "Unauthorized"));
         }
     }
+
+   
+    public function show($id)
+    {
+        $CourseModel = new Course();
+
+        $course = $CourseModel->find($id);
+
+        $authorized =  Courses::authorizationValidate();
+
+        
+
+        if ($authorized) {
+            if (!empty($course)) {
+
+                return json_encode(array("status" => 200,  "data" => $course));
+            } else {
+                return json_encode(array("status" => 404,  "data" => "not found course"));
+            }
+        } else {
+            return json_encode(array("status" => 401, "message" => "Unauthorized"));
+        }
+    }
+
+    public function update($id)
+    {
+
+        $authorized =  Courses::authorizationValidate();
+
+        $request = \Config\Services::request();
+        $validation = \Config\Services::validation();
+        $created_at = new DateTime();
+
+
+        if ($authorized) {
+            $data = array(
+                'name' => $request->getVar('name'),
+                'description' => $request->getVar('description'),
+
+            );
+
+            //validation html fields 
+            $validation->setRules([
+                'name' => 'required|string',
+                'description' => 'required|string'
+
+            ]);
+
+            $validation->withRequest($this->request)->run();
+            $errors = $validation->getErrors();
+            if ($validation->getErrors()) {
+
+
+                $res = array(
+                    "status" => 403,
+                    "details" => $errors
+                );
+
+                return json_encode($res, true);
+
+            } else {
+
+                $savedata = array(
+                    'name' => $data['name'],
+                    'description' => $data['description'],
+                    'updated_at' => $created_at->format("Y-m-d H:i:s"),
+                    
+                );
+
+               
+
+
+                $CourseModel = new Course();
+                $course = $CourseModel->find($id);
+                
+
+
+                if($_SESSION['id_student'] == $course['id_student']){
+                    $CourseModel->update($id,$savedata);
+                    if ($CourseModel) {
+
+
+                        return json_encode(array("status" => 201, "details" => "success, update course"));
+                    }else {
+                        return json_encode(array("status" => 409, "details" => "it could not update the course"));
+                    }
+                }else{
+                    return json_encode(array("status" => 403, "details" => "The client does not have access rights to the content"));
+                }
+                 
+            }
+        } else {
+            return json_encode(array("status" => 401, "message" => "Unauthorized"));
+        }
+    }
+
+    public function delete($id)
+    {
+
+        $authorized =  Courses::authorizationValidate();
+
+        if ($authorized) {
+           
+
+                $CourseModel = new Course();
+                $course = $CourseModel->find($id);
+                
+
+
+                if($_SESSION['id_student'] == $course['id_student']){
+                    $CourseModel->delete($id);
+                    if ($CourseModel) {
+
+
+                        return json_encode(array("status" => 201, "details" => "success, deleted course"));
+                    }else {
+                        return json_encode(array("status" => 409, "details" => "it could not delete the course"));
+                    }
+                }else{
+                    return json_encode(array("status" => 403, "details" => "The client does not have access rights to the content"));
+                }
+                 
+            }
+         else {
+            return json_encode(array("status" => 401, "message" => "Unauthorized"));
+        }
+
+    }
 }
+
